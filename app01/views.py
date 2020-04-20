dd447641b11d0697da7236c52907dc2676e73f09
duanzhenyan <2200225800@qq.com>
2020-04-20 16:24:30 +0800

    添加基本sms接口

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from utils.tencent.sms import send_sms_single
+from django.conf import settings
+import random
+
 
 # Create your views here.
+def send_sms(request):
+    """
+    发送短信
+    :param request:
+    ?tpl=login
+    ?tpl=register
+    :return:
+    """
+    tpl = request.GET.get("tpl")
+    phone = request.GET.get("phone")
+
+    tpl_id = settings.TENCENT_SMS_TEMPLATE[tpl]
+    if not tpl_id:
+        return HttpResponse("模板不存在")
+    if not phone:
+        return HttpResponse("手机号不能为空")
+    code = random.randrange(1000, 9999)
+    res = send_sms_single(phone, tpl_id, [code, ])
+    if res['result'] == 0:
+        return HttpResponse("成功")
+    else:
+        return HttpResponse(res['errmsg'])
--- tracer/settings.py
+++ tracer/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'majp+^3mzvuk0y*7$@5f#^pq*$0qcyksf#mf1ofjrz^j%ehrcg'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tracer.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -115,13 +110,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+##############腾讯sms配置##############
+TENCENT_SMS_APP_ID = 0
+TENCENT_SMS_APP_KEY = ""
+TENCENT_SMS_APP_KEY_SIGN = ""
 
-
+TENCENT_SMS_TEMPLATE = {
+    'login': 0,
+    'register': 0
+}
+##############腾讯sms配置##############
 try:
     from .local_settings import *
 except:
