dd94f8a894a4e9936f6d20fa02e4f8d9b6fb006f
duanzhenyan <2200225800@qq.com>
2020-05-13 17:12:26 +0800

    完善注册页面以及账号菜单栏

--- web/views/account.py
+++ web/views/account.py
@@ -2,7 +2,9 @@
 用户账号相关：注册、登录、重置密码
 """
 from django.shortcuts import render
+from web.forms.account import RegisterModelFrom
 
 
 def register(requset):
-    return render(requset, 'web/../templates/register.html')
+    form = RegisterModelFrom()
+    return render(requset, 'register.html', {"form": form})
